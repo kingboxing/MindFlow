dcd43fdb2eb37e478b969cf9972e4dfe58873e10
bojin <jinbo199188@gmail.com>
2024-10-16 23:56:17 +0800

    Create __init__.py
    update

---
+++ src/LinAlg/__init__.py
@@ -0,0 +1,9 @@
+# FERePack/src/LinAlg/__init__.py
+"""
+Linear algebra utilities for matrix/vector operations, assembling and other algebraic functions.
+"""
+
+from .MatrixOps import *
+from .MatrixAsm import *
+from .Utils import *
+from .VectorAsm import *
