60a6bc14109ffba7ac71a47fb593925def9c6025
bojin <jinbo199188@gmail.com>
2024-10-16 23:55:26 +0800

    Update __init__.py
    deleted

--- src/linalg/__init__.py
+++
@@ -1,9 +0,0 @@
-# FERePack/src/LinAlg/__init__.py
-"""
-Linear algebra utilities for matrix operations and other algebraic tasks.
-"""
-
-from .MatrixOps import *
-from .MatrixAsm import *
-from .Utils import *
-from .VectorAsm import *
