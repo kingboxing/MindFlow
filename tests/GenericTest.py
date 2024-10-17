76768eb8b5bfdff9e02c996d0866e66e25d63e58
bojin <jinbo199188@gmail.com>
2024-10-17 19:38:45 +0800

    general test

--- tests/GenericTest.py
+++ tests/GenericTest.py
@@ -1,48 +1,42 @@
-import matlab.engine
-import scipy.sparse as sp
-import numpy as np
-import time
-from scipy.io import mmread
-from src.Interface.Py2Mat import add_matlab_path, python2matlab, matlab2python
-from src.Params.Params import DefaultParameters
-from src.LinAlg.Utils import assemble_sparse, invert_diag_block_matrix
+from context import *
+from src.LinAlg.Utils import woodbury_solver
 
-# Start MATLAB engine
-eng = matlab.engine.start_matlab()
-add_matlab_path(eng)
-#%% eqn model
-m = mmread("data/mess/NSE_RE_500_lvl1_M.mtx").tocsr()
-a = mmread("data/mess/NSE_RE_500_lvl1_A.mtx").tocsr()
-g = mmread("data/mess/NSE_RE_500_lvl1_G.mtx").tocsr()
-b = mmread("data/mess/NSE_RE_500_lvl1_B.mtx")
-c = mmread("data/mess/NSE_RE_500_lvl1_C.mtx")
-k0 = mmread("data/mess/NSE_RE_500_lvl1_Feed0.mtx")
-model = {'A_': assemble_sparse([[a, g], [g.T, None]])}
-mg = sp.csr_matrix(g.shape)
-model['E_'] = assemble_sparse([[m,mg],[mg.T, None]])
-model['B'] = b
-model['C'] = c
-model['Q'] = np.identity(c.shape[0])
-model['R'] = np.identity(b.shape[1])
-model_mat = python2matlab(model)
-#%% params
-param = DefaultParameters().parameters['radiriccati_mmess']['riccati_solver']
-param['LDL_T'] = False
-param['eqn']['type']='T'
-#param['radi']['K0'] = python2matlab(k0)
-param['radi']['get_ZZt'] = False
-#%%
-output = eng.GRiccatiDAE2RADISolver(model_mat, param)
-out = matlab2python(output)
-#eng.quit()
+Ar = sp.csr_matrix([[4, 1, 5], [1, 3, 2], [2, 1, 4]], dtype=np.float64)
+Ai = sp.csr_matrix([[0, -1, -2], [1, 0, 4], [-1, 3, 5]], dtype=np.float64)
+A = Ar + 1j * Ai
+vr = np.array([1, 3, 4], dtype=np.float64)
+vi = np.array([2, 4, 6], dtype=np.float64)
+v = vr + 1j * vi
 
-y = out['Y']
-start_time = time.time()
-dinv = np.linalg.inv(y.toarray())
-end_time = time.time()
-print("Time to compute the inverse:", end_time - start_time, "seconds")
+U = np.random.rand(3, 1) + np.random.rand(3, 1) * 1j
+V = np.random.rand(3, 1) + np.random.rand(3, 1) * 1j
+b = np.random.rand(3) + np.random.rand(3) * 1j
+I = np.identity(3)
 
-start_time = time.time()
-yinv=invert_diag_block_matrix(y)
-end_time = time.time()
-print("Time to compute the inverse:", end_time - start_time, "seconds")
+Mat = {'U': U, 'V': V}
+trans = 'H'
+
+# Initialize the MatInv solver with the complex matrix
+# This example uses the 'mumps' solver, which is a common choice for LU factorization.
+mat_inv = InverseMatrixOperator(A, Mat=Mat, lusolver='mumps', trans=trans)
+
+# Solve the system A*x = b
+x = mat_inv.matvec(b)
+
+# Output the result
+print("Solution x:", x)
+
+# Verify by computing A*x and comparing it with b
+if trans == 'N':
+    A_complex = A + U @ V.T
+    Ax = A_complex.dot(x)
+elif trans == 'T':
+    A_complex = A.T + U @ V.T
+    Ax = A_complex.dot(x)
+elif trans == 'H':
+    A_complex = A.T.conjugate() + U @ V.T
+    Ax = A_complex.dot(x)
+
+print("b=Ax:", Ax)
+print("Original b:", b)
+assert np.allclose(Ax, b, atol=1e-6), "Ax does not match the original b."
